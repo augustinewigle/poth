cec37af9dbbae5f390aa44d668d85738dd3f2183
Augustine Wigle <augustine.wigle@gmail.com>
2024-04-17 13:35:24 +0200

    Made loo-sir mode modular. Add local_sir function

---
+++ R/local_sir.R
@@ -0,0 +1,205 @@
+#' Calculate the local SIR for a subset of treatments
+#'
+#' @param diffs Matrix of relative effects, must have column names that are the name of each treatment or trt_names must be specified
+#' @param ses Matrix of estimated standard errors for relative effects, must have column names that are the name of each treatment or trt_names must be specified
+#' @param trt_names optional; vector of treatment names matching order in diffs and sds
+#' @param largerbetter Logical, T if larger outcomes are good. Defauls is T
+#' @param samples a matrix or data.frame of MCMC samples, where rows are MCMC samples and columns are relative effects (relative to anchor) for treatments.
+#' must have column names that are the name of each treatment or trt_names must be specified
+#' @param subset A character vector of treatment names to consider as the set of competing treatments
+#' @param topx Calculate the local SIR for the top x treatments, as ranked by global SUCRA/P-scores
+#' @param bottomx Calculate the local SIR for the bottom x treatments, as ranked by global SUCRA/P-scores
+#' @param do_loo Should a leave-one-out analysis of the subset be performed? Default is F
+#' @returns A named list with two elements:
+#' df - a dataframe with the information used to draw the plot
+#' plot - a ggplot object
+#' @import ggplot2
+#' @importFrom stringr str_wrap
+#' @export
+local_sir <- function(diffs = NA,
+                      ses = NA,
+                      samples = NA,
+                      trt_names = NA,
+                      largerbetter,
+                      subset = NA,
+                      topx = NA,
+                      bottomx = NA,
+                      do_loo = F) {
+
+  # checks, define score type, check treatment names ------------------------------------
+
+  if(!is.null(dim(diffs))) {
+
+    bayes <- F # we are in Frequentist world
+
+    if(is.null(dim(ses))) {
+
+      stop("If diffs is specified then ses must also be specified")
+
+    }
+
+    score_type <- "P-score"
+    n <- nrow(diffs)
+
+    # name check
+    if(length(trt_names) != n) {
+
+      if(is.null(colnames(diffs))) {
+
+        stop("No treatment names specified. Treatment names are required for local_sir either through the trt_names argument or the column names of diffs")
+
+      }
+
+      trt_names <- colnames(diffs)
+
+    } else {
+
+      colnames(diffs) <- colnames(ses) <- trt_names
+
+    }
+
+  } else {
+
+    # Bayesian
+
+    bayes <- T
+
+    n <- ncol(samples)
+    score_type <- "SUCRA"
+
+    # name check
+    if(length(trt_names) != n) {
+
+      if(is.null(colnames(samples))) {
+
+        stop("No treatment names specified. Treatment names are required for local_sir either through the trt_names argument or the column names of diffs")
+
+      }
+
+      trt_names <- colnames(samples)
+
+    } else {
+
+      colnames(samples) <- trt_names
+
+    }
+
+  } # end of checks for treatment names
+
+
+  # Define subsets
+
+  if(length(subset)== 1) {
+
+    if(is.na(subset)) { # subset is NA
+
+      if(is.na(topx)&is.na(bottomx)) {
+
+        stop("One of subset, topx, or bottomx must be specified")
+
+      } else {
+
+        # Calculate ranks
+
+        if(bayes) {
+
+          rmat <- get_rank_prob_mat(samples = samples, largerbetter = largerbetter, trt_names = trt_names)
+          scores <- calc_sir(ranking_prob_mat = rmat, largerbetter, trt_names)$sucras
+
+        } else {
+
+          scores <- p_scores(diffs, ses, largerbetter, trt_names)
+
+        }
+
+        trt_order <- names(scores)[order(scores, decreasing = T)]
+
+        if(!is.na(topx)) { # define subset as the top x treatments
+
+          if(topx > n-1) {
+
+            stop('topx should be less than the number of treatments')
+
+          }
+
+          subset <- trt_order[1:topx]
+
+        } else if(!is.na(bottomx)) { # define subset as the top x treatments
+
+          if(bottomx > n-1) {
+
+            stop('bottomx should be less than the number of treatments')
+
+          }
+
+          subset <- trt_order[(n-bottomx+1):n]
+
+        }
+
+      }
+
+    }
+
+  } # end of defining subsets
+
+  # check that treatment names exist
+
+  if(any(!(subset %in% trt_names))) {
+
+    wrong_names <- subset[which(!(subset %in% trt_names))]
+
+    stop(paste0("The following treatments in subset were not found in the treatment names:\n    ",
+                paste(wrong_names, collapse = ",\n    "),
+                "\n  All treatments in subset must be found in the treatment names"))
+
+  }
+
+  # Calculate scores and SIR for the subset
+
+  if(bayes) {
+
+    # Calculate SUCRAs and global SIR
+    rps <- get_rank_prob_mat(samples[,subset], largerbetter, trt_names)
+    both <- calc_sir(ranking_prob_mat = rps, trt_names = colnames(rps))
+    scores <- both$sucras
+    sir <- both$sir
+
+    if(do_loo) {
+
+      loo <- loo_sir(samples = samples[,subset],
+                     largerbetter = largerbetter,
+                     trt_names = subset)
+    } else {
+
+      loo <- NULL
+
+    }
+
+  } else {
+
+    # Calculate P-scores
+    scores <- p_scores(diffs[subset, subset], ses[subset, subset], largerbetter, trt_names = subset)
+
+    # Regular SIR
+    sir <- calc_sir(sucras = scores, trt_names = subset)$sir
+
+    if(do_loo) {
+
+      loo <- loo_sir(diffs = diffs[subset, subset],
+                     ses = ses[subset, subset],
+                     largerbetter = largerbetter,
+                     trt_names = subset)
+    } else {
+
+      loo <- NULL
+
+    }
+
+  } # end of local scores and SIR
+
+  return(list(scores = scores,
+              score_type = score_type,
+              sir = sir,
+              loo = loo))
+
+}
--- R/loo_sir.R
+++ R/loo_sir.R
@@ -6,19 +6,25 @@
 #' @param largerbetter Logical, T if larger outcomes are good. Defauls is T
 #' @param samples a matrix or data.frame of MCMC samples, where rows are MCMC samples and columns are relative effects (relative to anchor) for treatments.
 #' must have column names that are the name of each treatment or trt_names must be specified
+#' @param plot_trt_names Logical; should treatment names be included in the plot? Default it T
 #' @returns A named list with two elements:
 #' df - a dataframe with the information used to draw the plot
 #' plot - a ggplot object
 #' @import ggplot2
-#' @importFrom stringr str_wrap
+#' @importFrom stringr str_trunc
 #' @export
 loo_sir <- function(diffs = NA,
                     ses = NA,
                     samples = NA,
                     trt_names = NA,
-                    largerbetter) {
+                    largerbetter,
+                    plot_trt_names = T) {
+
+  # Checks and definitions
 
   if(!is.null(dim(diffs))) { # we are in Frequentist world
+
+    bayes <- F
 
     if(is.null(dim(ses))) {
 
@@ -50,25 +56,9 @@ loo_sir <- function(diffs = NA,
 
     }
 
-    # Calculate P-scores
-    scores <- p_scores(diffs, ses, largerbetter, trt_names)
+  } else {
 
-    # Regular SIR
-    sir <-calc_sir(sucras = scores, trt_names = trt_names)$sir
-
-    # Calculate loo
-    # treatment order
-    id_order <- order(scores, decreasing = T)
-    ordered_diffs <- diffs[id_order, id_order]
-    ordered_ses <- ses[id_order, id_order]
-    # loo_pscores[[1]] corresponds to the p-scores we calculate if we leave out the number one ranked treatment based on the base p-scores
-    loo_pscores <- lapply(1:n, function(x) p_scores(ordered_diffs[-x,-x], ordered_ses[-x,-x], largerbetter, colnames(ordered_diffs)[-x]))
-    names(loo_pscores) <- colnames(ordered_diffs)
-
-    loo_sirs <- sapply(loo_pscores, function(x) calc_sir(sucras = x, trt_names = names(x))$sir)
-    names(loo_sirs) <- names(loo_pscores)
-
-  } else { # we are in Bayesian world
+    bayes <- T
 
     n <- ncol(samples)
     score_type <- "SUCRA"
@@ -94,6 +84,13 @@ loo_sir <- function(diffs = NA,
 
     }
 
+
+  } # end of checks and definitions
+
+  # Calculate scores and do LOO
+
+  if(bayes) {
+
     # Calculate SUCRAs and global SIR
     rps <- get_rank_prob_mat(samples, largerbetter, trt_names)
     both <- calc_sir(ranking_prob_mat = rps, trt_names = trt_names)
@@ -111,9 +108,30 @@ loo_sir <- function(diffs = NA,
     loo_sirs <- sapply(loo_rps, function(x) calc_sir(ranking_prob_mat = x)$sir)
     names(loo_sirs) <- colnames(ordered_samples)
 
-  }
+  } else {
+
+    # Calculate P-scores
+    scores <- p_scores(diffs, ses, largerbetter, trt_names)
+
+    # Regular SIR
+    sir <-calc_sir(sucras = scores, trt_names = trt_names)$sir
+
+    # Calculate loo
+    # treatment order
+    id_order <- order(scores, decreasing = T)
+    ordered_diffs <- diffs[id_order, id_order]
+    ordered_ses <- ses[id_order, id_order]
+    # loo_pscores[[1]] corresponds to the p-scores we calculate if we leave out the number one ranked treatment based on the base p-scores
+    loo_pscores <- lapply(1:n, function(x) p_scores(ordered_diffs[-x,-x], ordered_ses[-x,-x], largerbetter, colnames(ordered_diffs)[-x]))
+    names(loo_pscores) <- colnames(ordered_diffs)
+
+    loo_sirs <- sapply(loo_pscores, function(x) calc_sir(sucras = x, trt_names = names(x))$sir)
+    names(loo_sirs) <- names(loo_pscores)
+
+  } # end of scores and LOO
 
 
+  # Put everything together
 
   sir_res <- sir-loo_sirs
 
@@ -126,13 +144,16 @@ loo_sir <- function(diffs = NA,
                    SIR= sir)
 
   df$res_pos <- df$sir_res > 0
-  df$mag_order <- order(abs(df$sir_res))
+  # # df$mag_order <- order(abs(df$sir_res))
+  #
+  # df$xplot <- paste0(interaction(df$rank, df$trt_name, sep = "\n("), ")")
+  #
+  # df$xplot <- factor(df$xplot, levels = df$xplot[order(df$rank)])
+  #
+  # # df$short_names <- unlist(lapply(as.list(df$trt_name),
+  # #                          function(x) ifelse(strwidth(x) > 10, paste0(substr(x, start = 1, stop = 9), "."), x)))
 
-  df$xplot <- paste0(interaction(df$rank, df$trt_name, sep = "\n("), ")")
-
-  df$xplot <- factor(df$xplot, levels = df$xplot[order(df$rank)])
-
-  g <- ggplot(df, aes(x = xplot, y = sir_res, fill = res_pos, alpha = abs(sir_res))) +
+  g <- ggplot(df, aes(x = 1:n, y = sir_res, fill = res_pos, alpha = abs(sir_res))) +
     geom_col(col = "black") +
     coord_cartesian(ylim = c(-max(abs(df$sir_res)), max(abs(df$sir_res)))) +
     geom_hline(yintercept = 0) +
@@ -141,10 +162,28 @@ loo_sir <- function(diffs = NA,
     labs(y = ifelse(F, # can change if I want to go back to the ratio thing
                     expression(frac(SIR-SIR^"-i",SIR)),
                     expression(SIR-SIR^"-i")),
-         x = paste0("Global Rank Using ", score_type, "s (Treatment)"),
+         x = paste0("Global Rank Using ", score_type, "s"),
          fill = str_wrap(paste0("Contribution to SIR\n(Global SIR ", ifelse(sir < 0.001, "< 0.001", paste0("= ", round(sir, digits = 3))), ")"), width = 20)) +
     guides(alpha = "none") +
     theme_bw()
+
+  if(plot_trt_names) {
+
+    g <- g+ scale_x_continuous(sec.axis = sec_axis(~.,
+                                                   breaks = 1:n,
+                                                   labels = str_trunc(df$trt_name, width = 13),
+                                                   name = "Treatment Name"),
+                               breaks = 1:n, minor_breaks = NULL)+
+      theme(axis.text.x.top = element_text(angle = 90, hjust = 0.01, vjust = 0.4),
+            axis.title.x.top = element_blank())
+
+
+  } else {
+
+    g <- g + scale_x_continuous(breaks = 1:n, minor_breaks = NULL)
+
+  }
+
 
   return(list(plot = g,
               info = df))
