6c216890571c5ef1af9cd38e4b3a7c6087489c4a
Augustine Wigle <augustine.wigle@gmail.com>
2024-08-26 17:43:57 -0400

    Change axis labels to POTH

--- R/plot.loo.sir.R
+++ R/plot.loo.sir.R
@@ -1,8 +1,8 @@
 #' Plot results of leave-one-out method
-#' 
+#'
 #' @description
 #' Plot results of leave-one-out method for separation in ranking (SIR) metric
-#' 
+#'
 #' @param x R object of class \code{sir}.
 #' @param labels A logical indicating whether treatment names should be
 #'   shown in the plot.
@@ -34,38 +34,38 @@
 #'   event = list(event1, event2, event3), n = list(n1, n2, n3),
 #'   data = smokingcessation, sm = "OR")
 #' net1 <- netmeta(p1, random = FALSE)
-#' 
+#'
 #' # Leave-one-out method
 #' loo1 <- loo(sir(net1))
 #' loo1
 #' plot(loo1)
-#' 
+#'
 #' data(Senn2013)
 #' net2 <- netmeta(TE, seTE, treat1.long, treat2.long, studlab,
 #'                 data = Senn2013, sm = "MD", random = FALSE)
-#' 
+#'
 #' # Leave-one-out method (without sorting by ranking metric)
 #' loo2 <- loo(sir(net2), sort = FALSE)
 #' loo2
 #' plot(loo2)
 #' }
-#' 
+#'
 #' @method plot loo.sir
 #' @export
 
 plot.loo.sir <- function(x, labels = TRUE, digits = 3, ...) {
-  
+
   chkclass(x, "loo.sir")
-  
+
   res_pos <- x$residuals > 0
   sir <- attr(x, "sir")
   score_type <- attr(x, "score_type")
-  
+
   # Get rid of warning "no visible binding for global variable"
-  
+
   n <- residuals <- score_type <- NULL
   n.seq <- seq_len(nrow(x))
-  
+
   g <- ggplot(x,
               aes(x = n.seq, y = residuals,
                   fill = res_pos, alpha = abs(residuals))) +
@@ -74,15 +74,15 @@ plot.loo.sir <- function(x, labels = TRUE, digits = 3, ...) {
     geom_hline(yintercept = 0) +
     scale_fill_manual(breaks = c(FALSE, TRUE), values = c("red4", "darkgreen"),
                       labels = c("Reduces certainty", "Increases certainty")) +
-    labs(y = expression(SIR-SIR^"-i"),
-         x = paste0("Global Rank Using ", score_type, "s"),
-         fill = str_wrap(paste0("Contribution to SIR\n(global ",
+    labs(y = expression(r["POTH"]~"(j)"),
+         x = paste0("Place in Hierarchy"),
+         fill = str_wrap(paste0("Contribution to POTH (",
                                 formatPT(sir, digits = digits, lab = TRUE,
-                                         labval = "SIR"),
+                                         labval = "POTH"),
                                 ")"), width = 20)) +
     guides(alpha = "none") +
     theme_bw()
-  
+
   if (labels) {
     g <- g + scale_x_continuous(
       sec.axis = sec_axis(~.,
@@ -96,6 +96,6 @@ plot.loo.sir <- function(x, labels = TRUE, digits = 3, ...) {
   }
   else
     g <- g + scale_x_continuous(breaks = n.seq, minor_breaks = NULL)
-  
-  g  
+
+  g
 }
